b196c7041e0826b1c3920f2a43b554b871ba99c4
Pavel Polcr <pavel.polcr@seznam.cz>
2021-01-22 20:12:36 +0100

    pangram + isogram exc done

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        word = word.ToLower();
+        bool isIsogram = true;
+        List<char> chars = new List<char>();
+        for (int i = 0; i < word.Length; i++)
+        {
+            if ((int)word[i] >= 97 && (int)word[i] <= 122) // is it letter?)
+            {
+                if (chars.Contains(word[i])) // does list of chars contain this?
+                {
+                    isIsogram = false; //its already in list - not isogram
+                }
+                else
+                {
+                    chars.Add(word[i]); // not already in list = add there
+                }
+            }
+
+            
+        }
+        return isIsogram;
+    }
+}
--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -4,6 +4,33 @@ public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        string chnInput = "";
+        bool isPangram = true;
+        input = input.ToLower();
+        input = input.Trim();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if ((int)input[i] >= 97 && (int)input[i] <= 122) // is it letter?
+            {
+                chnInput += input[i];
+            }
+        }
+        
+        int[] lettersOcc = new int[26]; // counter for 25 letters of ascii alphabet
+        for (int i = 0; i < chnInput.Length; i++)
+        {
+            
+                lettersOcc[(int)chnInput[i] - 97] += 1; //add 1 for every occurence of letter to corresponding field of lettersOccArray(expl. acc of 'a' stored under index 0 )
+           
+           
+        }
+        for (int i = 0; i < lettersOcc.Length; i++) // check if every letter occured exactly one time.
+        {
+            if(lettersOcc[i] ==0)
+            {
+                isPangram = false;
+            }
+        }
+        return isPangram;
     }
 }
